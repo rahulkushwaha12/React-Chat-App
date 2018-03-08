b21ada19c37214a6e47feba4a8a27425637d0cf6
Rahul Kushwaha <kushwaharesonance@gmail.com>
2018-03-09 01:08:13 +0530

    added functionality to left side bar now we can create new channel and send msg and added members to channel

--- app/src/components/messenger.js
+++ app/src/components/messenger.js
@@ -4,6 +4,8 @@ import avatar from '../images/avatar.png';
 import {OrderedMap} from 'immutable';
 import _ from 'lodash';
 import ObjectID from '../helpers/objectid';
+import SearchUser from './search-user';
+import moment from 'moment';
 
 export default class Messenger extends Component{
     constructor(props){
@@ -11,45 +13,88 @@ export default class Messenger extends Component{
         this.state={
             height:window.innerHeight,
             newMessage: 'Hello there...',
+            searchUser: "",
+            showSearchUser:false,
         }
         this._onResize = this._onResize.bind(this); 
-        this.addTestMessages = this.addTestMessages.bind(this);
+        //this.addTestMessages = this.addTestMessages.bind(this);
         this.handleSend = this.handleSend.bind(this);
         this.renderMessage = this.renderMessage.bind(this);
+        this.scrollMessageToBottom = this.scrollMessageToBottom.bind(this);
+        this.onCreateChannel = this.onCreateChannel.bind(this);
+        this.renderChannelTitle = this.renderChannelTitle.bind(this);
+    }
+    renderChannelTitle(channel={}){
+        const {store} = this.props;
+        const members = store.getMembersFromChannel(channel);
+        const names=[];
+       members.forEach((user)=>{
+           const name = _.get(user,'name');
+           names.push(name);
+       })
+       return <h2>{_.join(names,',')}</h2>
+    }
+    onCreateChannel(){
+        const {store} = this.props;
+        const channelId = new ObjectID().toString();
+        const channel={
+            _id: channelId,
+            title: "",
+            lastMessage: "",
+            members:new OrderedMap(),
+            messages:new OrderedMap(),
+            isNew: true,
+            created: new Date(),
+        };
+        store.onCreateNewChannel(channel);
+    }
+    scrollMessageToBottom(){
+        if(this.messagesRef){
+            this.messagesRef.scrollTop = this.messagesRef.scrollHeight;
+        }
     }
     renderMessage(message){
-        return <p dangerouslySetInnerHTML={{__html: _.get(message,'body')}}></p>
+        const text = _.get(message,'body','');
+        const html = _.split(text, '\n').map((m,key)=>{
+            return <p key={key} dangerouslySetInnerHTML={{__html: m}}/>
+        })
+        return html;
     }
     handleSend(){
         const {newMessage} = this.state;
         const {store} = this.props;
-        const messageId = new ObjectID().toString();
-        const channel = store.getActiveChannel();
-        const channelId = _.get(channel, '_id', null);
-        const currentUser = store.getCurrentUser();
-        const message = {
-            _id:messageId,
-            channelId: channelId,
-            body:newMessage,
-            auth:_.get(currentUser,'name',null),
-            avatar: avatar,
-            me:true,
+        if(_.trim(newMessage).length){
+            const messageId = new ObjectID().toString();
+            const channel = store.getActiveChannel();
+            const channelId = _.get(channel, '_id', null);
+            const currentUser = store.getCurrentUser();
+            const message = {
+                _id:messageId,
+                channelId: channelId,
+                body:newMessage,
+                auth:_.get(currentUser,'name',null),
+                avatar: avatar,
+                me:true,
+            }
+            store.addMessage(messageId,message);
+            this.setState({
+                newMessage: '',
+            })
         }
-        store.addMessage(messageId,message);
-        this.setState({
-            newMessage: '',
-        })
+        
     }
     _onResize(){
         this.setState({
             height:window.innerHeight
         });
     }
+    componentDidUpdate(){
+        this.scrollMessageToBottom();
+    }
     componentDidMount(){
         window.addEventListener('resize',this._onResize);
-        this.addTestMessages();
     }
-    addTestMessages(){
+   /* addTestMessages(){
         const {store} = this.props;
 
         //create test msgs
@@ -81,6 +126,7 @@ export default class Messenger extends Component{
                     '3': true,
                  }),
                  messages:new OrderedMap(),
+                 created: new Date(),
              }
              const msgId = `${c}`;
              const moreMsgId = `${c+1}`;
@@ -88,7 +134,7 @@ export default class Messenger extends Component{
              newChannel.messages = newChannel.messages.set(moreMsgId,true);
              store.addChannel(c,newChannel);
          }
-    }
+    }*/
     componentWillUnmount(){
         window.removeEventListener('resize',this._onResize)
     }
@@ -107,11 +153,35 @@ export default class Messenger extends Component{
         <div style={style} className="app-messenger">
         <div className="header">
             <div className="left">
-                <div className="actions">
-                    <button>new message</button>
-                </div>
+                <button className="left-action" ><i className="icon-settings-streamline-1" /></button>
+                <button onClick={this.onCreateChannel} className="right-action" ><i className="icon-edit-modify-streamline" /></button>
+                <h2>Messenger</h2>
             </div>
-            <div className="content"><h2>{_.get(activeChannel,'title','')}</h2></div>
+            <div className="content">
+
+                {_.get(activeChannel,'isNew')?<div className="toolbar">
+                    <label>To:</label>
+                    <input placeholder="Type name of person..." onChange={(event)=>{
+                        const searchUserText = _.get(event,'target.value');
+                        this.setState({
+                            searchUser: searchUserText,
+                            showSearchUser: true,
+                        });
+                    }}
+                    type="text" value={this.state.searchUser} />
+                    {this.state.showSearchUser?<SearchUser 
+                    onSelect={(user)=>{
+                        this.setState({
+                            showSearchUser: false,
+                            searchUser:'',
+                        },()=>{
+                            const userId = _.get(user,'_id');
+                            const channelId = _.get(activeChannel,'_id');
+                            store.addUserToChannel(channelId,userId);
+                        })
+                    }} search={this.state.searchUser} store={store}/>:null}
+                </div>:<h2>{_.get(activeChannel,'title','')}</h2>}
+            </div>
             <div className="right">
                 <div className="user-bar">
                     <div className="profile-name">Rahul Kushwaha</div>
@@ -131,7 +201,7 @@ export default class Messenger extends Component{
                                 <img src={avatar} alt="" />
                             </div>
                             <div className="channel-info">
-                                <h2>{channel.title}</h2>
+                                <h2>{this.renderChannelTitle(channel)}</h2>
                                 <p>{channel.lastMessage}</p>
                             </div>
                         </div>
@@ -141,7 +211,7 @@ export default class Messenger extends Component{
             </div>
         </div>
         <div className="content">
-            <div className="messages">
+            <div ref={(ref)=>this.messagesRef=ref} className="messages">
                 {messages.map((message,index)=>{
                     return (
                         <div key={index} className={classNames('message',{'me':message.me})}>
@@ -170,7 +240,7 @@ export default class Messenger extends Component{
                         }
                     }} onChange={(event)=>{
                         this.setState({newMessage: _.get(event, 'target.value')});
-                    }} value= {this.state.newMessage} placeholder="Write your message..." />
+                    }} value={this.state.newMessage} placeholder="Write your message..." />
                 </div>
                 <div className="actions">
                 <button onClick={this.handleSend} className="send">Send</button>
@@ -178,23 +248,23 @@ export default class Messenger extends Component{
             </div>
         </div>
         <div className="sidebar-right">
-                <h2 className="title">Members</h2>
+                {members.size >0 ? <div><h2 className="title">Members</h2>
                 <div className="members">
                     {members.map((member,key)=>{
                         return(
                             <div key={key} className="member">
                                 <div className="user-image">
-                                    <img src={avatar} alt="" />
+                                    <img src={_.get(member,'avatar')} alt="" />
                                 </div>
                                 <div className="member-info">
                                     <h2>{member.name}</h2>
-                                    <p>Joined: 4 days ago.</p>
+                                    <p>Joined: {moment(member.created).fromNow()}</p>
                                 </div>
                             </div>
                         )
                     })};
 
-                </div>
+                </div></div>:null}
         </div>
         </div>
         </div>
--- app/src/store.js
+++ app/src/store.js
@@ -2,9 +2,11 @@ import {OrderedMap} from 'immutable';
 import _ from 'lodash';
 
 const users = OrderedMap({
-    '1': {_id:'1', name:"Rahul", created: new Date()},
-    '2': {_id:'2', name:"Bhai", created: new Date()},
-    '3': {_id:'3', name:"Bhagat", created: new Date()},
+    '1': {_id:'1', name:"Rahul Kushwaha", created: new Date(), avatar: 'https://api.adorable.io/avatars/100/abott@rahul.png'},
+    '2': {_id:'2', name:"Ashutosh Udeniya", created: new Date(), avatar: 'https://api.adorable.io/avatars/100/abott@bhai.png'},
+    '3': {_id:'3', name:"Shubham Bhagat", created: new Date(), avatar: 'https://api.adorable.io/avatars/100/abott@bhagat.png'},
+    '4': {_id:'4', name:"Ankit Soni", created: new Date(), avatar: 'https://api.adorable.io/avatars/100/abott@soni.png'},
+    '5': {_id:'5', name:"Vijay Prajapati", created: new Date(), avatar: 'https://api.adorable.io/avatars/100/abott@vijay.png'},
 });
 export default class Store{
     constructor(appComponent){
@@ -14,23 +16,55 @@ export default class Store{
         this.activeChannelId = null;
         this.user = {
             _id:'1',
-            name: 'Rahul',
+            name: 'Rahul Kushwaha',
             created: new Date(),
         }
+    }
+    addUserToChannel(channelId,userId){
+        const channel = this.channels.get(channelId);
+        if(channel)
+        {
+            channel.members = channel.members.set(userId,true);
+            this.channels = this.channels.set(channelId,channel);
+            this.update();
+        }
+    }
+    searchUsers(search=""){
+        let searchItems = new OrderedMap();
+        if(_.trim(search).length){
+            users.filter((user)=>{
+                const name = _.get(user,'name').toLowerCase();
+                const userId = _.get(user,'_id');
+                if(_.includes(name,search.toLowerCase())){
+                    searchItems = searchItems.set(userId,user);
+                }
+            })
+        }
+        return searchItems.valueSeq();
+    }
+    onCreateNewChannel(channel={}){
+        const channelId = _.get(channel,'_id');
+        this.addChannel(channelId, channel);
+        this.setActiveChannelId(channelId);
+
     }
     getCurrentUser(){
         return this.user;
     }
     getMembersFromChannel(channel){
-        let members = [];
+        let members = new OrderedMap();
         if(channel)
         {
             channel.members.map((value,key)=>{
-                const member  = users.get(key);
-                members.push(member);
-            });
+                const user  = users.get(key);
+                const loggedUser = this.getCurrentUser();
+                if(_.get(loggedUser,'_id')!==_.get(user,'_id')){
+                    members = members.set(key,user);
+                }
+                });
+                
         }
-        return members;
+        return members.valueSeq();
     }
     getMessagesFromChannel(channel){
         let messages = [];
@@ -56,7 +90,9 @@ export default class Store{
         const channelId = _.get(message,'channelId');
         if(channelId)
         {
-            const channel = this.channels.get(channelId);
+            let channel = this.channels.get(channelId);
+            channel.isNew = false;
+            channel.lastMessage = _.get(message, 'body','');
             channel.messages = channel.messages.set(id, true);
             this.channels = this.channels.set(channelId, channel);
         }
@@ -70,6 +106,8 @@ export default class Store{
         this.update();
     }
     getChannels(){
+        //
+        this.channels = this.channels.sort((a,b)=>a.created<b.created);
         return this.channels.valueSeq();
     }
     update(){
